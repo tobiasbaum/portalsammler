5c278506f3581d83c9a565c8acd59ba2a2e9ba47
Tobias Baum <tbaum@tntinteractive.de>
2013-11-12 23:30:06 +0100

    PDF-Anzeige

--- src/main/java/de/tntinteractive/portalsammler/gui/PDFViewer.java
+++ src/main/java/de/tntinteractive/portalsammler/gui/PDFViewer.java
@@ -1,31 +1,93 @@
 package de.tntinteractive.portalsammler.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Rectangle2D;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
 
-public class PDFViewer extends JFrame
-{
-    private final Image image;
+public class PDFViewer extends JFrame {
 
-    public PDFViewer(String title, PDFFile pdfFile)
-    {
+    private static final long serialVersionUID = 843311728636015602L;
+
+    private final PDFFile pdfFile;
+    private int currentPage;
+    private final JLabel posLabel;
+    private final JLabel contentLabel;
+
+    public PDFViewer(String title, PDFFile pdfFile) {
         super(title);
 
-        final int numpages = pdfFile.getNumPages();
+        this.pdfFile = pdfFile;
+        this.currentPage = 1;
 
-        final PDFPage page = pdfFile.getPage(1);
+        this.setLayout(new BorderLayout());
+
+        this.contentLabel = new JLabel();
+        this.contentLabel.setVerticalAlignment(JLabel.TOP);
+        final JScrollPane scrollPane = new JScrollPane(this.contentLabel);
+        this.add(scrollPane, BorderLayout.CENTER);
+
+        this.posLabel = new JLabel();
+        final JButton backwardButton = new JButton("<");
+        backwardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PDFViewer.this.backward();
+            }
+        });
+        final JButton forwardButton = new JButton(">");
+        forwardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PDFViewer.this.forward();
+            }
+        });
+
+        final JPanel navigationPanel = new JPanel();
+        navigationPanel.setLayout(new BoxLayout(navigationPanel, BoxLayout.LINE_AXIS));
+        navigationPanel.add(backwardButton);
+        navigationPanel.add(this.posLabel);
+        navigationPanel.add(forwardButton);
+
+        this.showCurrentPage();
+        this.add(navigationPanel, BorderLayout.SOUTH);
+
+        this.pack();
+    }
+
+    private void backward() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+            this.showCurrentPage();
+        }
+    }
+
+    private void forward() {
+        if (this.currentPage < this.pdfFile.getNumPages()) {
+            this.currentPage++;
+            this.showCurrentPage();
+        }
+    }
+
+    private void showCurrentPage() {
+        this.posLabel.setText(this.currentPage + " / " + this.pdfFile.getNumPages());
+
+        final PDFPage page = this.pdfFile.getPage(this.currentPage);
 
         final Rectangle2D r2d = page.getBBox();
-
         double width = r2d.getWidth();
         double height = r2d.getHeight();
         width /= 72.0;
@@ -34,14 +96,10 @@ public class PDFViewer extends JFrame
         width *= res;
         height *= res;
 
-        this.image = page.getImage((int) width,(int) height, r2d, null, true, true);
+        final Image image = page.getImage((int) width,(int) height, r2d, null, true, true);
+        this.contentLabel.setIcon(new ImageIcon(image));
 
-        final JLabel label = new JLabel(new ImageIcon(this.image));
-        label.setVerticalAlignment(JLabel.TOP);
-
-        this.setContentPane(new JScrollPane(label));
-
-        this.pack();
+        this.validate();
     }
 
 }
